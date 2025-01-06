10a9f31ff9eedf05aaf255b0884901865962e52e
Diocleniax789 <julioramdom123@gmail.com>
2025-01-05 23:49:32 -0300

    se ha creado a forma de practica en el boton que al hacer click varias veces luego se detiene

--- script/batallas.js
+++ script/batallas.js
@@ -1,2 +1,13 @@
-const boton = document.getElementsByClassName('boton');
-console.log(boton);
+const botones = document.querySelectorAll('.boton');
+
+for(let boton of botones) {
+	let acum = 0;
+	boton.addEventListener("click", sumarNumero = () => {
+		if(acum >= 0 && acum <= 10){
+			boton.textContent  = acum;
+			acum += 1;
+		} else {
+			boton.textContent = 'FINALIZADO!';
+		}
+	});
+}
