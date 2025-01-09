4606a66ebd076dda5919489651c6965bb6aa1522
Diocleniax789 <julioramdom123@gmail.com>
2025-01-09 00:06:11 -0300

    se ha solucionado el problema del script que hacia que solo la ultima tarjeta funcione

--- script/batallas.js
+++ script/batallas.js
@@ -1,17 +1,14 @@
-const botones = document.querySelectorAll(".boton");
+const botones = document.querySelectorAll('.boton');
 
+for (let boton of botones) {
+  boton.addEventListener('click', () => {
+    const targetId = boton.getAttribute('data-target');
+    const pantallaEmergente = document.getElementById(targetId);
+    pantallaEmergente.style.display = 'flex';
 
-for(let boton of botones) {
-	boton.addEventListener('click', abrirPantalla = () => {
-		const targetId = boton.getAttribute("data-target");
-		const pantallaEmergente = document.getElementById(targetId);
-		const botonCerrar = document.querySelector('.boton-cerrar');
-		
-		pantallaEmergente.style.display = 'flex';
-		
-		botonCerrar.addEventListener('click', cerrarPantalla = () => {
-      		pantallaEmergente.style.display = 'none';
-  		});
-	}); 
+    const botonCerrar = pantallaEmergente.querySelector('.boton-cerrar');
+    botonCerrar.addEventListener('click', () => {
+      pantallaEmergente.style.display = 'none';
+    });
+  });
 }
-
