c1c93b6c9c25f4103f01de79e428380ecf05a6ff
Diocleniax789 <julioramdom123@gmail.com>
2025-01-11 00:58:32 -0300

    se ha agregado un nuevo archivo js

--- script/batallas.js
+++ script/batallas.js
@@ -1,13 +1,18 @@
 const botones = document.querySelectorAll('.boton');
+let sonido = new Audio('audio/button2.mp3');
 
-for (let boton of botones) {
+sonido.load();
+
+for (let boton of botones) { 
   boton.addEventListener('click', () => {
+    sonido.play();
     const targetId = boton.getAttribute('data-target');
     const pantallaEmergente = document.getElementById(targetId);
     pantallaEmergente.style.display = 'flex';
 
     const botonCerrar = pantallaEmergente.querySelector('.boton-cerrar');
     botonCerrar.addEventListener('click', () => {
+      sonido.play();
       pantallaEmergente.style.display = 'none';
     });
   });
---
+++ script/index.js
@@ -0,0 +1,10 @@
+let botones = document.querySelectorAll('.boton');
+let sonido = new Audio('audio/button2.mp3');
+
+sonido.load();
+
+for(let boton of botones) {
+	boton.addEventListener('mouseover',() => {
+		sonido.play();
+	});
+}
