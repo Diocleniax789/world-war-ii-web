19fe4237cf46df166d56aa8623432a51722e36b6
Diocleniax789 <julioramdom123@gmail.com>
2025-01-11 01:15:40 -0300

    se ha agregado sonido a los botones del menu

--- script/index.js
+++ script/index.js
@@ -1,10 +1,9 @@
 let botones = document.querySelectorAll('.boton');
 let sonido = new Audio('audio/button2.mp3');
 
-sonido.load();
-
-for(let boton of botones) {
-	boton.addEventListener('mouseover',() => {
+for(let boton of botones) {	
+	boton.addEventListener('mouseover',botonSonido = () => {		
+		sonido.load();
 		sonido.play();
 	});
 }
