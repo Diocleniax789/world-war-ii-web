04847e2d3d04069cd9e9015a98da3aa730d556dd
Diocleniax789 <julioramdom123@gmail.com>
2025-01-13 16:01:19 -0300

    se han añadido la funcionalidad a los botones de cerrar

--- script/historias.js
+++ script/historias.js
@@ -1,10 +1,18 @@
 let imagenes = document.querySelectorAll('.img-texto');
+let botonesCerrar = document.querySelectorAll('.boton-cerrar');
 
 for(let imagen of imagenes) {
 	let dataImg = imagen.getAttribute('data-img');	
 	let modalImg = document.getElementById(dataImg);
+	let botonCerrar  = modalImg.getAttribute('class');
 
 	imagen.addEventListener('click', () => {
 		modalImg.style.display = 'flex';
 	});
+
+	for(let botonCerrar of botonesCerrar) {
+		botonCerrar.addEventListener('click',() => {
+			modalImg.style.display = 'none';
+		});
+	}
 }
