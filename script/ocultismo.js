f34555c1dbae349a838c457c7a64020ce04e1c42
Diocleniax789 <julioramdom123@gmail.com>
2025-01-17 00:51:14 -0300

    se han agregado audios para los botones

--- script/ocultismo.js
+++ script/ocultismo.js
@@ -1,12 +1,19 @@
 imagen0 = document.getElementById('img-0');
 ventanaModal = document.querySelector('.ventana-modal');
 botonCerrar = document.getElementById('boton-cerrar');
+sonidoBotonAbrir = new Audio('audio/button3.mp3');
+sonidoBotonCerrar = new Audio('audio/button4.mp3');
+
+sonidoBotonAbrir.load();
+sonidoBotonCerrar.load();
 
 imagen0.addEventListener('click', () => {
+	sonidoBotonAbrir.play();
 	ventanaModal.style.display = 'flex';
 });
 
 botonCerrar.addEventListener('click', () => {
+	sonidoBotonCerrar.play();
 	ventanaModal.style.display = 'none';
 });
 
