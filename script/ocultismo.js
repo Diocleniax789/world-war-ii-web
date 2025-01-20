db60ffc20eb1c4bc990d687179113471e50f8a45
Diocleniax789 <julioramdom123@gmail.com>
2025-01-19 21:47:44 -0300

    se han agregado nuevos codigos, pero falta solucionar un problema de scroll

--- script/ocultismo.js
+++ script/ocultismo.js
@@ -22,16 +22,30 @@ let libros = document.querySelectorAll('.img-texto');
 let botonAtras = document.getElementById('boton-atras');
 let botonAdelante = document.getElementById('boton-adelante');
 
-
 for(let libro of libros) {
 	let dataLibro = libro.getAttribute('data-libro');
 	let libroImagen = document.getElementById(dataLibro);
 }
 
+
+let incrementar = 0;
+
 botonAtras.addEventListener('click', () => {
 	libreria.scrollLeft -= 300;
+	incrementar -= 1;
+	if (incrementar <= 0) {
+		botonAtras.style.opacity = "0.5";
+	} else {
+		
+	}
 });
 
 botonAdelante.addEventListener('click', () => {
 	libreria.scrollLeft += 300;
+	incrementar += 1;
+	console.log(incrementar);
+	if (incrementar >= 0) {
+		botonAtras.style.opacity = "1";
+	}
 });
+
