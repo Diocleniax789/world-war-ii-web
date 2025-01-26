b06d85bd2481095bf348f53a55adbd99087a14b5
Diocleniax789 <julioramdom123@gmail.com>
2025-01-26 15:59:38 -0300

    se han agregado nuevos sonidos para los botones

--- script/batallas.js
+++ script/batallas.js
@@ -1,18 +1,20 @@
 const botones = document.querySelectorAll('.boton');
-let sonido = new Audio('audio/button2.mp3');
+let sonidoAbrir = new Audio('audio/button3.mp3');
+let sonidoCerrar = new Audio('audio/button4.mp3');
 
-sonido.load();
+sonidoAbrir.load();
+sonidoCerrar.load();
 
 for (let boton of botones) { 
   boton.addEventListener('click', () => {
-    sonido.play();
+    sonidoAbrir.play();
     const targetId = boton.getAttribute('data-target');
     const pantallaEmergente = document.getElementById(targetId);
     pantallaEmergente.style.display = 'flex';
 
     const botonCerrar = pantallaEmergente.querySelector('.boton-cerrar');
     botonCerrar.addEventListener('click', () => {
-      sonido.play();
+      sonidoCerrar.play();
       pantallaEmergente.style.display = 'none';
     });
   });
